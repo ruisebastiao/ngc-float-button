c9c7b109f875b491303ebceaf429c96ba4398cd7
Rui Sebastiao <ruisebastiao@gmail.com>
2017-09-06 22:51:05 +0100

    Update ngc-float-item-button.component.ts
    using ng-content will allow more flexibility on the button content

--- components/ngc-float-item-button.component.ts
+++ components/ngc-float-item-button.component.ts
@@ -18,7 +18,7 @@ import {
           <div class="content" [style.display]="content ? 'block' : 'none'">{{content}}</div>
         </div>
         <a class="fab-item">
-           <md-icon class="material-content-icon"> {{icon}} </md-icon>
+          <ng-content select="[fab-content]"></ng-content>
         </a>
     </div>
   `,
